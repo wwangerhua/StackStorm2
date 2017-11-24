34951228cf95c65b69023f6ab53406d0637d126d
wangerhua <841471411@qq.com>
2017-11-24 18:13:31 +0800

    上传代码

---
+++ config.js
@@ -0,0 +1,14 @@
+'use strict';
+angular.module('main')
+  .constant('st2Config', {
+
+    // hosts: [{
+    //   name: 'Dev Env',
+    //   url: '//172.168.50.50:9101'
+    // }, {
+    //   name: 'Express',
+    //   url: '//172.168.90.50:9101',
+    //   auth: true
+    // }]
+
+  });
